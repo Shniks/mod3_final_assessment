334048a0d8459634d61719e793ac937c5a954ac9
Nikhil Shahi <shniks@gmail.com>
2018-05-30 11:58:18 -0600

    Added second part for feature 3 and completed assessment.

--- spec/requests/player_request_spec.rb
+++ spec/requests/player_request_spec.rb
@@ -17,6 +17,25 @@ describe 'As a user' do
     end
   end
 
+  describe 'When I send a get request to /api/v1/games/1' do
+    scenario 'I can see the details of my game' do
+      VCR.use_cassette('get_game_details_after_post') do
+        p1 = create(:user, name: 'Nikhil')
+        p2 = create(:user, name: 'Sergio')
+        game = Game.create(player_1: p1, player_2: p2)
+        p1.plays.create(game: game, word: 'hello', score: 17)
+        p2.plays.create(game: game, word: 'bird', score: 16)
+
+        get "/api/v1/games/#{game.id}"
+
+        game = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(200)
+        expect(game).to eq(expected_response)
+      end
+    end
+  end
+
   # Everything the same as above but it only allows valid words to be played. If an invalid word is played the response is:
   # {"message": "foxez is not a valid word."}
 
@@ -32,4 +51,20 @@ describe 'As a user' do
       end
     end
   end
+end
+
+def expected_response
+  {
+  "game_id":1,
+  "scores": [
+    {
+      "user_id":1,
+      "score":17
+    },
+    {
+      "user_id":2,
+      "score":16
+    }
+  ]
+}
 end
