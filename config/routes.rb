ab0fe5301dcaa0a1da142fa9ed4c0b1d603182d2
Nikhil Shahi <shniks@gmail.com>
2018-05-30 10:59:17 -0600

    Passing test for Feature 3, to post a play, added routes and the create action with strong params to the plays controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root "welcome#index"
-  get '/search', to: "search#index"
+  get '/search', to: 'search#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :games do
+        resources :plays, only: [:create]
+      end
+    end
+  end
 end
--- spec/requests/player_request_spec.rb
+++ spec/requests/player_request_spec.rb
@@ -5,14 +5,15 @@ describe 'As a user' do
     scenario 'It creates a play' do
       VCR.use_cassette('post_game_play') do
         game = create(:game)
-        data = {user_id: game.player_1.id, word: 'foxes'}
+        data = {user_id: game.player_1.id, word: 'Turing'}
         post "/api/v1/games/#{game.id}/plays", params: {play: data}
 
         play = Play.last
 
         expect(response.status).to eq(201)
         expect(play.user_id).to eq(game.player_1.id)
-        expect(play.word).to eq('foxes')
+        expect(play.word).to eq('Turing')
       end
     end
   end
+end
